51f5552c248277e25695f637e472dbb2a480eb6c
Jeffrey Cline <jcline@microsoft.com>
2017-02-28 13:00:39 -0800

    Moved the files back to .NET from the Core NuGet file, updated codesign to reflect this.

--- tools/codesign_dotnet.cmd
+++ tools/codesign_dotnet.cmd
@@ -37,6 +37,10 @@ if %errorlevel% neq 0 goto :EOF
 mkdir %csu-output%
 if %errorlevel% neq 0 goto :EOF
 
+REM Copy files to be signed to the tosign folder
+xcopy /q /y /R %root%bindings\dotnet\dotnet-binding\Microsoft.Azure.IoT.Gateway\bin\x86\Release\Microsoft.Azure.IoT.Gateway.dll %csu-source%
+if %errorlevel% neq 0  goto :EOF
+
 xcopy /q /y /R %root%bindings\dotnet\dotnet-binding\PrinterModule\bin\x86\Release\PrinterModule.dll %csu-source%
 if %errorlevel% neq 0  goto :EOF
 
@@ -46,6 +50,10 @@ if %errorlevel% neq 0  goto :EOF
 REM Auto-sign the managed dlls placed in the "tosign" Folder 
 csu.exe /s=True /w=True /i=%root%build\ToSign /o=%root%build\Signed /c2=401 /d="SN Signing Azure IoT SDK .NET Gateway binaries"
 if %errorlevel% neq 0 goto :EOF
+
+REM Copy the siged dlls from the signed folder to their original locations
+xcopy /q /y /R  %csu-output%\Microsoft.Azure.IoT.Gateway.dll  %root%bindings\dotnet\dotnet-binding\Microsoft.Azure.IoT.Gateway\bin\x86\Release
+if %errorlevel% neq 0  goto :EOF
 
 xcopy /q /y /R %csu-output%\PrinterModule.dll %root%bindings\dotnet\dotnet-binding\PrinterModule\bin\x86\Release 
 if %errorlevel% neq 0  goto :EOF
